6d7880deaa24ee4eee308eca2df608a55a88d1c6
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-22 09:43:28 -0300

    refactor: included banner component

--- projeto-5/src/app/app.module.ts
+++ projeto-5/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
