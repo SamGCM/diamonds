a08c45d7dd2a4b9dc13f87266e0ac984851c7528
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-22 07:23:35 -0300

    rebuild app with scss

--- projeto-5/src/app/app.module.ts
+++ projeto-5/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BannerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
