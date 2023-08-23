a7c5cfba20e92c65bb787200d17ab51b305cb144
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-23 15:05:43 -0300

    remove environment file

--- projeto-5/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'http://my-prod-url',
-//   API_KEY_TMDB: import.meta.env.API_KEY_TMDB,
-//   API_TOKEN_TMDB: import.meta.env.API_TOKEN_TMDB,
-// };
