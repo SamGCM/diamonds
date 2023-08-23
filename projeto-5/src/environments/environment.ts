4b528cfdf74fbbbd09fb8372df47b74d146751a7
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-23 14:41:28 -0300

    build: new env

---
+++ projeto-5/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://my-prod-url',
+  API_KEY_TMDB: "4f02138867b6579a00d18c6e60a0c4ac",
+  API_TOKEN_TMDB: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjAyMTM4ODY3YjY1NzlhMDBkMThjNmU2MGEwYzRhYyIsInN1YiI6IjY0ZTU3NzNiZTBjYTdmMDBjNjY1YzhiZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8leaZspWrwwhSfT38DcS91WoHwlUKC_yx-t1FLqbmIQ"
+};
