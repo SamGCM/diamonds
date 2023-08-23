9ddde67957457c1a66cd90b4cbec8b62759bd051
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-22 23:54:36 -0300

    data: categories

---
+++ projeto-5/src/data/categories.ts
@@ -0,0 +1,26 @@
+export const dataCategories = [
+  {
+    title: "Ação",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJEtvJY1-liLYpm47gHK2qPnX_rY7Z8MWhVw&usqp=CAU",
+    gif: "https://i.pinimg.com/originals/2a/69/e0/2a69e0b52926d4c27a217baeba398553.gif",
+    href: "#"
+  },
+  {
+    title: "Animação",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLTWf6qb_3vT80R0FU5DUjxRqMBEXhBem4Kudl6DSwPSNCN-O6NO_o_kM2TpznpWm_Np8&usqp=CAU",
+    gif: "https://media.tenor.com/uqrBXF4xYFkAAAAd/luca-disney.gif",
+    href: "#"
+  },
+  {
+    title: "SciFi",
+    img: "https://www.centralcomics.com/wp-content/uploads/2021/09/Finch_banner.jpg",
+    gif: "https://64.media.tumblr.com/9aec90558070b0457a8ad836fa646149/21db359c0396ec38-9e/s540x810/c7e92ba7fe971a7fff3ebfaaff23be9004dda82d.gif",
+    href: "#"
+  },
+  {
+    title: "Fantasia",
+    img: "https://sm.ign.com/ign_br/screenshot/default/11896238-1310595408966779-1756262868619829961-n_wa98.png",
+    gif: "https://64.media.tumblr.com/019a34b8b4ca7465882897573dcfd6b9/tumblr_pi3i4g04C41rhs5nco3_540.gif",
+    href: "#"
+  },
+]
