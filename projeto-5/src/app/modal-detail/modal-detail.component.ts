126a1e6a09931bce0097179d6ef9b28c3c828af6
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-29 12:02:51 -0300

    refactor: removed responsibility to make req

--- projeto-5/src/app/modal-detail/modal-detail.component.ts
+++ projeto-5/src/app/modal-detail/modal-detail.component.ts
@@ -8,29 +8,11 @@ import { urlBaseImage } from 'src/constants/urlBaseImage';
   templateUrl: './modal-detail.component.html',
   styleUrls: ['./modal-detail.component.scss']
 })
-export class ModalDetailComponent implements OnInit, OnChanges {
+export class ModalDetailComponent {
 
-  @Input() movieId: string;
-
-  public movieDetail: MovieDetail;
+  @Input() movieDetail: MovieDetail;
   public urlBaseImage: string = urlBaseImage;
 
-  constructor(
-    private moviesService: MoviesService
-  ){}
-
-  ngOnInit(): void {  }
-
-  ngOnChanges() {
-    if(this.movieId !== "") {
-      this.getMovieDetail(parseInt(this.movieId))
-    }
-  }
-
-  getMovieDetail(id: number) {
-    this.moviesService.getMovieDetail(id).subscribe((response: any) =>{
-      this.movieDetail = response;
-    })
-  }
+  constructor(){}
 
 }
