07178156490b14d842a357a1e0244bee631e1427
Samuel Cruz <samuelcruz052@gmail.com>
2023-08-29 12:02:14 -0300

    feat: included new endpoints

--- projeto-5/src/app/services/movies.services.ts
+++ projeto-5/src/app/services/movies.services.ts
@@ -1,39 +1,102 @@
 import { HttpClient } from "@angular/common/http";
-import { Movie } from "../shared/movie.model";
-import { MovieDetail } from "../shared/movieDetail.model";
 import { Injectable } from '@angular/core';
-import { Observable, Subscription } from "rxjs";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class MoviesService {
-  readonly apiURL : string = `https://api.themoviedb.org/3/movie`;
+  readonly apiURL : string = `https://api.themoviedb.org/3`;
+
+  readonly HEADERS = {
+    Authorization: 'Bearer ' + import.meta.env["NG_APP_API_TOKEN_TMDB"]
+  }
 
   constructor(private http : HttpClient) {
   }
 
   public getMovieDetail(id: number): Observable<any> {
 
-    return this.http.get(`${ this.apiURL }/${id}?language=pt-BR`, {
-      headers: {
-        Authorization: 'Bearer ' + import.meta.env["NG_APP_API_TOKEN_TMDB"]
+    return this.http.get(`${ this.apiURL }/movie/${id}`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR"
+      }
+    })
+  }
+
+  public getMoviesNowPlaying(): Observable<any> {
+
+    return this.http.get(`${ this.apiURL }/movie/now_playing`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1
+      }
+    })
+  }
+
+  public getMoviesUpcoming(): Observable<any> {
+
+    return this.http.get(`${ this.apiURL }/movie/upcoming`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1
       }
     })
   }
 
   public getMoviesPopulars(): Observable<any> {
 
-    return this.http.get(`${ this.apiURL }/popular?language=pt-BR&page=1`, {
-      headers: {
-        Authorization: 'Bearer ' + import.meta.env["NG_APP_API_TOKEN_TMDB"]
+    return this.http.get(`${ this.apiURL }/movie/popular`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1
       }
     })
   }
 
   getMoviesTopRated(): Observable<any> {
-    return this.http.get(`${ this.apiURL }/top_rated?language=pt-BR&page=1`, {
-      headers: {
-        Authorization: 'Bearer ' + import.meta.env["NG_APP_API_TOKEN_TMDB"]
+    return this.http.get(`${ this.apiURL }/movie/top_rated`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1
       }
     })
   }
+
+  public searchMovie(query: string): Observable<any> {
+
+    return this.http.get(`${ this.apiURL }/search/movie`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1,
+        include_adult: false,
+        sort_by: "popularity.desc",
+        query: query
+      }
+    })
+  }
+
+  public exploreMoviesGenre(query: string): Observable<any> {
+    let queryCategory = query ? "" + query : ""
+
+    return this.http.get(`${ this.apiURL }/discover/movie`, {
+      headers: this.HEADERS,
+      params: {
+        language: "pt-BR",
+        page: 1,
+        include_adult: false,
+        sort_by: "popularity.desc",
+        with_genres: queryCategory
+      }
+    })
+  }
+
+}
+
+interface IExploreMovies {
+  category?: string
 }
