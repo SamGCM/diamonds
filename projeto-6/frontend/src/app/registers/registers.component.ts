ef7a6c5db3305f32475277858fa3c2e31f11f21a
Samuel Cruz <samuelcruz052@gmail.com>
2023-09-12 09:30:01 -0300

    feat: included modal and actions

--- projeto-6/frontend/src/app/registers/registers.component.ts
+++ projeto-6/frontend/src/app/registers/registers.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ICandidate } from '../interfaces/candidate';
+import { IRegister } from '../interfaces/register';
 
 @Component({
   selector: 'app-registers',
@@ -6,12 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./registers.component.scss']
 })
 export class RegistersComponent {
-  dataTable = [{
+  dataTable: IRegister[] = [{
+    id: "13611513",
+    status: "Aprovado",
     candidate: {
+      dateOfBirth: "27/07/1997",
+      id: "1231651",
+      document: 75365943185,
+      phone: "859871198987",
+      listSkills: [{
+        name: "Teamwork",
+        level: "intermediary"
+      }],
       name: "Jorge",
       function: "Desenvolvedor",
-      schooling: "Superior incompleto",
+      schooling: "Superior Incompleto",
       email: "jorge@ghotmail.com"
     }
   }]
+
+  showModal: boolean = false;
+  candidateSelected: ICandidate;
+
+
+  toggleModal(){
+    this.showModal = !this.showModal;
+  }
+
+  setCandidate(candidate: ICandidate) {
+    this.candidateSelected = candidate;
+    this.toggleModal()
+  }
+
+
+  approveRegistration(registerId: string) {
+
+  }
+
+  rejectRegistration(registerId: string) {
+
+  }
 }
