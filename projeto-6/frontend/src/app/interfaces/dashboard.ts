e2c888b1b3362dffd494066b3a43b41dcbd8fd56
Samuel Cruz <samuelcruz052@gmail.com>
2023-09-12 09:29:43 -0300

    feat: create interfaces

---
+++ projeto-6/frontend/src/app/interfaces/dashboard.ts
@@ -0,0 +1,22 @@
+export interface IDashboard {
+  charts: IChart
+}
+
+export interface IChart {
+  schooling: {
+    Analfabeto?: number,
+    FundamentalCompleto?: number,
+    MedioIncompleto?: number,
+    MedioCompleto?: number,
+    SuperiorCompleto?: number,
+    SuperiorIncompleto?: number,
+    Mestrado?: number,
+    Doutorado?: number,
+    Ignorado?: number,
+  },
+  statusRegisters: {
+    aguardando: number,
+    reprovados: number,
+    aprovados: number,
+  }
+}
