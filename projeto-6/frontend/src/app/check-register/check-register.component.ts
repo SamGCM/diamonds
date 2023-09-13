a1faccf282f593fd49e1e7535fe41cee116b62dc
Samuel Cruz <samuelcruz052@gmail.com>
2023-09-13 11:29:25 -0300

    refactor: included dialog in alerts

--- projeto-6/frontend/src/app/check-register/check-register.component.ts
+++ projeto-6/frontend/src/app/check-register/check-register.component.ts
@@ -1,13 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-check-register',
   templateUrl: './check-register.component.html',
   styleUrls: ['./check-register.component.scss']
 })
-export class CheckRegisterComponent {
+export class CheckRegisterComponent implements OnInit {
   showModal = false;
+  dataDialog: {
+    title: string,
+    description: string,
+    actionName?: string
+  }
+  typeDialog: "alert" | "success" | "error" = "alert"
+  actionDialog: Function | null;
+
+  formGetCandidate: any;
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit() {
+    this.formGetCandidate = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  onSubmit() {
+    if(!this.formGetCandidate.valid) {
+      this.formGetCandidate.markAllAsTouched();
+      this.handleModal("Alerta", "Preencha todos os campos", "alert")
+    }
+
+  }
+
+  handleModal(title: string, description: string, type: "alert" | "success" | "error", action?: {actionName: string, actionDialog: Function}) {
+    if(action) {
+      this.dataDialog = {
+        title: title,
+        description: description,
+        actionName: action.actionName
+      }
+
+      this.actionDialog = action.actionDialog
+    }
+
+    else {
+      this.dataDialog = {
+        title: title,
+        description: description
+      }
+    }
+
+    this.typeDialog = type
+    this.toggleModal()
+  }
+
   toggleModal(){
     this.showModal = !this.showModal;
   }
+
+  get email() { return this.formGetCandidate.get('email'); }
+  get password() { return this.formGetCandidate.get('password'); }
 }
